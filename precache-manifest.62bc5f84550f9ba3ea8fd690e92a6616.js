54a368816bfbc2604e7c60730f3323c9f699fc26
Keerthyb <keerthyb@IN-Keerthyb.local>
2019-04-02 18:00:11 +0530

    Updates

---
+++ precache-manifest.62bc5f84550f9ba3ea8fd690e92a6616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b85b46eb36839a93144",
+    "url": "/new/static/css/main.b697abd8.chunk.css"
+  },
+  {
+    "revision": "9b85b46eb36839a93144",
+    "url": "/new/static/js/main.a574762e.chunk.js"
+  },
+  {
+    "revision": "203f4010d004ffdf2b66",
+    "url": "/new/static/js/runtime~main.dee46077.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/new/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5702a7c9b76f873c2b379d12e8aba2ff",
+    "url": "/new/index.html"
+  }
+];
